fc335369b9eae68bd01b742c544eae1a104fcfe6
Robin Kang <kangdaehyun@gmail.com>
2017-03-08 15:38:49 -0800

    20170308 15:38

--- JframeTest/src/Jframetest.java
+++ JframeTest/src/Jframetest.java
@@ -12,35 +12,43 @@ import javax.swing.filechooser.*;
 
 public class Jframetest {			
 	public static void main(String[] args) throws IOException{
-	   //  Jframe_Test jt = new Jframe_Test();
+	    Jframe_Test jt = new Jframe_Test();
+	    Jframe_Test jt2 = new Jframe_Test();
+	    
+	    jt2.Jframe_initial();	    
 		System.out.println("실행되나?");
 		String[] vari = new String[2];
 		vari[0] = "xls";
 		vari[1] = "xls1";
-				
+		/*		
 		excel_excutor ec = new excel_excutor();
 		String[][] temp = ec.excel_excutor_result(vari[0]);
 		
 		System.out.println(temp[2][1]);
 		
 		Db_operation dbo = new Db_operation(temp);
-		
+		*/
 		
 	}
 }
 class  Jframe_Test extends JFrame implements ActionListener {
 
 	private JFrame frame = new JFrame();
+	private JFrame frame1 = new JFrame();
 	private JPanel buttonPane = new JPanel();
 	private JFileChooser jfc = new JFileChooser();
 	private JLabel jbl = new JLabel();
 	private JButton openButton = new JButton("Open");
 	private JButton saveButton = new JButton("Save");
+	private JButton company_info_upload = new JButton("1.Upload Company Info");
+	private JButton product_info_upload = new JButton("2.Upload Product Info");
+	private JButton info_download = new JButton("3.Download Information");
 	
 	public Jframe_Test() {
 		
         Container contentPane = frame.getContentPane();
 		contentPane.setBackground(Color.white);
+		
 		
 		buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.X_AXIS));
 		buttonPane.add(openButton);
@@ -65,7 +73,19 @@ class  Jframe_Test extends JFrame implements ActionListener {
 		frame.setSize(300, 200);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// JOptionPane.showMessageDialog(null, "경고경고", "경고입니다", JOptionPane.ERROR_MESSAGE); 
 		
+	}
+	
+	public void Jframe_initial(){
+		 
+		Container contentPane = frame1.getContentPane();
+		 contentPane.setBackground(Color.white);
+		 
+		 frame1.setSize(400,500);
+		 frame1.setVisible(true);
+		 frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		 
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -74,9 +94,11 @@ class  Jframe_Test extends JFrame implements ActionListener {
 				File file = jfc.getSelectedFile();
 				boolean file2 = jfc.accept(file);				
 				String file_Name = file.getName();
-				jbl.setText("선택한 파일: " + file_Name);
+				String file_addr = file.getPath();
+				jbl.setText("선택한 파일: " + file_addr);
 				System.out.println(file_Name);
 				System.out.println(file2);
+				
 				
 			}
 		}
