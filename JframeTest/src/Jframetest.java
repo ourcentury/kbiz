02137ef8ce6f3e64e87b3fe239973a294004a29d
Robin Kang <kangdaehyun@gmail.com>
2017-03-20 00:07:53 -0700

    commit 20170320 00:07

--- JframeTest/src/Jframetest.java
+++ JframeTest/src/Jframetest.java
@@ -2,7 +2,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.ComponentOrientation;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;	
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -56,10 +60,11 @@ class  Jframe_Test extends JFrame implements ActionListener {
     
 	private JButton openButton = new JButton("Open");
 	private JButton saveButton = new JButton("Save");
+	private JButton initButton = new JButton("Home");
 	private JButton company_info_upload = new JButton("1.Upload Company Info");
 	private JButton product_info_upload = new JButton("2.Upload Product Info");
 	private JButton info_download = new JButton("3.Download Information");
-	private GridLayout experimetalLayout = new GridLayout(2,0);
+	private GridLayout experimetalLayout = new GridLayout(2,1);
 	private Container contentPane = frame.getContentPane();
 	
 	
@@ -127,28 +132,136 @@ class  Jframe_Test extends JFrame implements ActionListener {
 		 
 	}
 	
-	public void Jframe_empty(Container contentPane, String pane_recog){	   	
+	public void Jframe_empty(String pane_recog){	   	
 	   if (pane_recog=="initial"){
-		  contentPane.remove(logoPane);
-		  contentPane.remove(menuSelectPane);
+		   contentPane.removeAll();
+		  //contentPane.remove(logoPane);
+		  //contentPane.remove(menuSelectPane);
+		  
 		  System.out.println("remove");
 	   }
 	   System.out.println("revalidate");
-	   frame.invalidate();
-	   frame.validate();
-	   frame.repaint();
+	   //frame.invalidate();
+	   //frame.validate();
+	   //frame.repaint();
 	   
 	}
 	public void Jframe_upload(){
 		
+		JPanel label_panel = new JPanel();
+		JPanel blank_panel = new JPanel();
+		JPanel init_btn_Panel = new JPanel();
+		
+		contentPane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		//GridBagConstraints c1 = new GridBagConstraints();
+		//GridBagConstraints c2 = new GridBagConstraints();
+		
+		
+	    //c.ipadx = 0;
+		c.fill = GridBagConstraints.FIRST_LINE_START;
+		c.anchor = GridBagConstraints.BASELINE_LEADING;		
+		c.gridx=0;
+		c.gridy=1;
+		c.gridwidth = 3;	
+		//c.gridheight = 3;
+		c.weightx = 0.7;
+	    c.weighty = 0.5;
+		//c.gridheight = 2;		
+		//c.gridwidth = 100;
+		
+		jbl.setText("파일선택 전입니다.");
+		label_panel.add(jbl);		
+		contentPane.add(label_panel,c);
+		/*
+		c.gridx=1;
+		c.gridy=0;
+		c.gridwidth = 1;
+		contentPane.add(blank_panel,c);
+		*/
+		buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.X_AXIS));
+		buttonPane.add(openButton);
+		buttonPane.add(saveButton);
+		buttonPane.setBackground(Color.white);
+		
+		openButton.addActionListener(this);
+		saveButton.addActionListener(this);
+		//buttonPane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		//buttonPane.setBorder(BorderFactory.createEmptyBorder(0, 150, 0, 0));
+		
+		/* init으로 돌아가는 버튼 */
+		init_btn_Panel.add(initButton);
+		//c.anchor = GridBagConstraints.EAST;
+				
+		c.gridx=0;
+		c.gridy=2;
+		//c.ipadx = 3;
+		c.gridwidth = 1;	
+		c.weightx = 0.15;
+		c.weighty = 0.3;
+		//c.gridwidth = 0;
+				
+		//c.insets = new Insets(20,0,0,0);
+		contentPane.add(init_btn_Panel, c);
+		
+		//c.anchor = GridBagConstraints.LAST_LINE_END;		
+		c.gridx=2;
+		c.gridy=2;
+		c.gridwidth = 1;
+		//c.weightx = 0;
+		//c.gridwidth = 0;
+		//c.insets = new Insets(20,50,0,0);
+		
+		contentPane.add(buttonPane, c);
+		
+		jfc.setFileFilter(new FileNameExtensionFilter("xls", "xls"));
+		jfc.setMultiSelectionEnabled(false);
+		
+		frame.invalidate();
+		frame.validate();
+		frame.repaint();
+
 	};
-	
+	public void Jframe_download(){
+		//JPanel textfield_panel = new JPanel();
+		
+		
+		JTextArea txtfield = new JTextArea("바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야바보야");
+		JScrollPane textfield_panel = new JScrollPane(txtfield);
+		
+		//JTextField txtfield = new JTextField(1);
+		//textfield_panel.add(txtfield);
+		
+		
+		//textfield_panel.setPreferredSize(new Dimension(250,250));
+		contentPane.add(textfield_panel, BorderLayout.CENTER);
+		frame.invalidate();
+		frame.validate();
+		frame.repaint();
+		
+		
+		
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		/* 메뉴에서 업로드하는 메뉴를 선택 했을 때 */		
 		if (e.getSource() == upload_comp){			
 			String recog = "initial";
-			Jframe_empty(contentPane, recog);
+			Jframe_empty(recog);
+			System.out.println("1");
+			Jframe_upload();
+			System.out.println("@");
 		}
+		/* 메뉴에서 서치하는 메뉴를 선택 했을 때 */
+		if (e.getSource() == download_info){
+			String recog = "initial";
+			Jframe_empty(recog);
+			Jframe_download();
+			System.out.println("$$");
+			
+		}
+		
 		/* 오픈 버튼 선택시 */
 		if (e.getSource() == openButton){
 			if(jfc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
@@ -157,14 +270,20 @@ class  Jframe_Test extends JFrame implements ActionListener {
 				String file_Name = file.getName();
 				String file_addr = file.getPath();
 				jbl.setText("선택한 파일: " + file_addr);
+				file_Name.lastIndexOf(".") ;
+				String file_name_new = file_Name.substring(file_Name.lastIndexOf(".")+1);
+				
 				System.out.println(file_Name);
-				System.out.println(file2);
+				System.out.println(file_name_new);
 			}
 		}
 	   /* 세이브 버튼 선택시 */	
 		if (e.getSource() == saveButton){
 			if (jfc.accept(jfc.getSelectedFile()) == true) {
 				System.out.println(jfc.accept(jfc.getSelectedFile()));
+				File file = jfc.getSelectedFile();
+				String file_Name = file.getName();
+				System.out.println(file_Name);
 			}
 			System.out.println("파일세이브선택");
 		}
